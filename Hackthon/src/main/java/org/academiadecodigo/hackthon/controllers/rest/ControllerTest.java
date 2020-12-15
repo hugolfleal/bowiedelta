296e960f41f6c9595ba39fa4bc8dbc08d9f2e589
Hugo Leal <hugolfleal@gmail.com>
2020-12-15 17:22:40 +0000

    skeleton hello

---
+++ Hackthon/src/main/java/org/academiadecodigo/hackthon/controllers/rest/ControllerTest.java
@@ -0,0 +1,22 @@
+package org.academiadecodigo.hackthon.controllers.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ControllerTest {
+
+    @RequestMapping(value = "/teste", method = RequestMethod.GET)
+    public ResponseEntity<String> sayHello() {
+
+        String hello = "hello";
+
+        return new ResponseEntity<>(hello, HttpStatus.OK);
+    }
+
+
+}
